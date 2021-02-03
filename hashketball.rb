6013476404b7bd5e2f160b9f27c6a5be31a9a0a5
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 20:56:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,7 @@ end
 def big_shoe_rebounds
 biggest_shoe= 0
 rebounds = 0
-  
+
   game_hash.each do |team, team_location|
     team_location[:players].each do |player|
       if player [:shoe] > biggest_shoe

6ac08f10ff12996b5e4883cef55f51d1c9f45bb5
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 20:29:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,7 +173,7 @@ def player_numbers (team_search)
 
 
   if game_hash[:home][:team_name] == team_search
-    while counter <game_hashv[:home][:players].values.length
+    while counter <game_hash[:home][:players].values.length
       game_hash[:home][:players][counter][:number]
       binding.pry
       counter+=1

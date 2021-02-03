97cd8dbde6841fc04045a8199fb5199f26e2edc8
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 19:48:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ def player_numbers (team_search)
     game_hash.each do |team, team_location|
       team_location[:players].each do |numbers|
         home_jersey_numbers << numbers[:number]
-        # binding.pry
+        binding.pry
       end
     end
     home_jersey_numbers
